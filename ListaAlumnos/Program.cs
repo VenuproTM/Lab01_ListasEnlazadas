3f23d8c9acca41b373330512289d05cd163c17a3
Alexis00 <alexisensala996@gmail.com>
2026-08-31 17:11:18 -0500

    Parte 2: codigo autogenerado, resgistro de nombres y edad, insercion al final

--- ListaAlumnos/Program.cs
+++ ListaAlumnos/Program.cs
@@ -10,14 +10,23 @@ class Alumno
     public int[,] Notas = new int[2, 2]; // [curso, nota1/nota2]
     public Alumno Siguiente; // puntero al siguiente nodo
 }
-
-
 class Program // Variables globales y menú principal
 {
     static Alumno cabeza = null;   // primer nodo de la lista
     static Alumno ultimo = null;   // último nodo (para insertar al final)
     static int contador = 1000;    // generador de código autogenerado
     static int totalAlumnos = 0;
+
+    static string GenerarCodigo()
+    {
+        string codigo = "N" + contador.ToString() + "P";
+
+        if (contador < 9999)
+            contador++;
+
+        return codigo;
+    }
+
     static void Main()
     {
         int opcion;
@@ -25,8 +34,6 @@ class Program // Variables globales y menú principal
         {
             Console.WriteLine("\n===== MENÚ - LISTA DE ALUMNOS =====");
             Console.WriteLine("[1] Registrar");
-            Console.WriteLine("[2] Mostrar lista de alumnos");
-            Console.WriteLine("[3] N° Alumnos registrados");
             Console.WriteLine("[4] Salir");
             Console.Write("Seleccione una opción: ");
             opcion = int.Parse(Console.ReadLine());
@@ -34,11 +41,39 @@ class Program // Variables globales y menú principal
             switch (opcion)
             {
                 case 1: RegistrarAlumno(); break;
-                case 2: MostrarListaAlumnos(); break;
-                case 3: ContarAlumnos(); break;
                 case 4: Console.WriteLine("Saliendo del sistema..."); break;
                 default: Console.WriteLine("Opción inválida."); break;
             }
         } while (opcion != 4);
+    }
+
+    static void RegistrarAlumno()
+    {
+        Alumno nuevo = new Alumno();
+
+        nuevo.Codigo = GenerarCodigo();
+
+        Console.Write("Ingrese nombres y apellidos: ");
+        nuevo.Nombres = Console.ReadLine();
+
+        Console.Write("Ingrese edad: ");
+        nuevo.Edad = int.Parse(Console.ReadLine());
+
+        nuevo.Siguiente = null;
+
+        if (cabeza == null)
+        {
+            cabeza = nuevo;
+            ultimo = nuevo;
+        }
+        else
+        {
+            ultimo.Siguiente = nuevo;
+            ultimo = nuevo;
+        }
+
+        totalAlumnos++;
+
+        Console.WriteLine($"\nAlumno registrado. Código: {nuevo.Codigo}");
     }
 }
